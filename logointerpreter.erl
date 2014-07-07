ac51844492296653763ab1821a2ef1b3de33b11a
Bastien Duplessier <bastien.duplessier@gmail.com>
2014-07-07 19:37:22 +0200

    Read and compute arithmetics expressions

--- logointerpreter.erl
+++ logointerpreter.erl
@@ -2,7 +2,7 @@
 -export([test/0, execute/1]).
 
 test() ->
-    execute("VE FPOS [ 0 0 ] BC AV 100 TD 90 AV 100 TD 90 AV 100").
+    execute("AV (23 + 43 * (34 + 54)) / 10").
 
 execute(String) ->
     {ok, Tokens, _} = logoscanner:string(String),
@@ -18,17 +18,17 @@ run(_) -> true.
 
 % Basic commands
 run_instruction({av, [Argument]}) ->
-    io:format("Going Forward - ~p !\n", [Argument]);
+    io:format("Going Forward - ~p !\n", [compute_aexpr(Argument)]);
 run_instruction({td, [Argument]}) ->
-    io:format("Turning right - ~p !\n", [Argument]);
+    io:format("Turning right - ~p !\n", [compute_aexpr(Argument)]);
 run_instruction({tg, [Argument]}) ->
-    io:format("Turning left - ~p !\n", [Argument]);
+    io:format("Turning left - ~p !\n", [compute_aexpr(Argument)]);
 run_instruction({rec, [Argument]}) ->
-    io:format("Going Backward - ~p !\n", [Argument]);
-run_instruction({fpos, Arguments}) ->
-    io:format("Positioning - ~p !\n", [Arguments]);
+    io:format("Going Backward - ~p !\n", [compute_aexpr(Argument)]);
+run_instruction({fpos, [X, Y]}) ->
+    io:format("Positioning - ~p ~p !\n", [compute_aexpr(X), compute_aexpr(Y)]);
 run_instruction({fcap, [Argument]}) ->
-    io:format("Setting angle - ~p !\n", Argument);
+    io:format("Setting angle - ~p !\n", compute_aexpr(Argument));
 run_instruction({ve, []}) ->
     io:format("Cleaning screen\n");
 run_instruction({mt, []}) ->
@@ -40,8 +40,19 @@ run_instruction({lc, []}) ->
 run_instruction({bc, []}) ->
     io:format("Lower pencil\n");
 run_instruction({fcc, [Argument]}) ->
-    io:format("Change color to ~p\n", [Argument]);
+    io:format("Change color to ~p\n", [compute_aexpr(Argument)]);
 % Default : error
 run_instruction(Instruction) ->
     io:format("Bad Instruction :  ~p !\n", [Instruction]).
 
+
+compute_aexpr({{Operator}, A, B}) ->
+    {int, ComputedA} = compute_aexpr(A),
+    {int, ComputedB} = compute_aexpr(B),
+    {int, trunc(compute(Operator, ComputedA, ComputedB))};
+compute_aexpr(Value) -> Value.
+
+compute(plus, A, B) -> A + B;
+compute(minus, A, B) -> A - B;
+compute(multiply, A, B) -> A * B;
+compute(divide, A, B) -> A / B.
