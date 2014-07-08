0ee1b366b597cb321e2feb6388a539064dcdbec9
Bastien Duplessier <bastien.duplessier@gmail.com>
2014-07-08 17:48:55 +0200

    Create fakedrawer

---
+++ logofakedrawer.erl
@@ -0,0 +1,29 @@
+-module(logofakedrawer).
+-export([av/1, td/1, tg/1, rec/1, fpos/1, fcap/1, ve/1,
+	 mt/1, ct/1, lc/1, bc/1, fcc/1]).
+
+% Basic commands
+av([X]) ->
+    io:format("Going Forward - ~p !\n", [X]).
+td([X]) ->
+    io:format("Turning right - ~p !\n", [X]).
+tg([X]) ->
+    io:format("Turning left - ~p !\n", [X]).
+rec([X]) ->
+    io:format("Going Backward - ~p !\n", [X]).
+fpos([X, Y]) ->
+    io:format("Positioning - ~p ~p !\n", [X, Y]).
+fcap(X) ->
+    io:format("Setting angle - ~p !\n", [X]).
+ve([]) ->
+    io:format("Cleaning screen\n").
+mt([]) -> 
+    io:format("Show turtle\n").
+ct([]) ->
+    io:format("Hide turtle\n").
+lc([]) ->
+    io:format("Lift pencil\n").
+bc([]) ->
+    io:format("Lower pencil\n").
+fcc([X]) ->
+    io:format("Change color to ~p\n", [X]).
