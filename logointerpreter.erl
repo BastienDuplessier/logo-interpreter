38d60902bb7cd0ecaa9c4b662238f06795865b28
Bastien Duplessier <bastien.duplessier@gmail.com>
2014-07-09 11:23:09 +0200

    Externalising drawer

--- logointerpreter.erl
+++ logointerpreter.erl
@@ -17,33 +17,19 @@ run([]) ->
 run(_) -> true.
 
 % Basic commands
-run_instruction({av, [Argument]}) ->
-    io:format("Going Forward - ~p !\n", [compute_aexpr(Argument)]);
-run_instruction({td, [Argument]}) ->
-    io:format("Turning right - ~p !\n", [compute_aexpr(Argument)]);
-run_instruction({tg, [Argument]}) ->
-    io:format("Turning left - ~p !\n", [compute_aexpr(Argument)]);
-run_instruction({rec, [Argument]}) ->
-    io:format("Going Backward - ~p !\n", [compute_aexpr(Argument)]);
-run_instruction({fpos, [X, Y]}) ->
-    io:format("Positioning - ~p ~p !\n", [compute_aexpr(X), compute_aexpr(Y)]);
-run_instruction({fcap, [Argument]}) ->
-    io:format("Setting angle - ~p !\n", compute_aexpr(Argument));
-run_instruction({ve, []}) ->
-    io:format("Cleaning screen\n");
-run_instruction({mt, []}) ->
-    io:format("Show turtle\n");
-run_instruction({ct, []}) ->
-    io:format("Hide turtle\n");
-run_instruction({lc, []}) ->
-    io:format("Lift pencil\n");
-run_instruction({bc, []}) ->
-    io:format("Lower pencil\n");
-run_instruction({fcc, [Argument]}) ->
-    io:format("Change color to ~p\n", [compute_aexpr(Argument)]);
+run_instruction({Symbol, ArgumentList}) ->
+    ComputedArguments = compute_arguments(ArgumentList),
+    Drawer = drawer(),
+    Drawer:do(Symbol, ComputedArguments);
 % Default : error
 run_instruction(Instruction) ->
     io:format("Bad Instruction :  ~p !\n", [Instruction]).
+
+compute_arguments(List) -> compute_arguments(List, []).
+compute_arguments([H|T], Result) ->
+    Argument = compute_aexpr(H),
+    compute_arguments(T, [Argument|Result]);
+compute_arguments([], Result) -> Result.
 
 
 compute_aexpr({{Operator}, A, B}) ->
@@ -56,3 +42,5 @@ compute(plus, A, B) -> A + B;
 compute(minus, A, B) -> A - B;
 compute(multiply, A, B) -> A * B;
 compute(divide, A, B) -> A / B.
+
+drawer() -> logofakedrawer.
